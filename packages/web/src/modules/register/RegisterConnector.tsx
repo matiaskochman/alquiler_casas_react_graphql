59cd3408b018e916ce0fa00415b5e5b6ecea0bf9
matias <matiaskochman@gmail.com>
2019-01-14 13:51:55 -0300

    [ADD] added package controller

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
-import { RegisterView } from "./ui/RegisterView";
+import { RegisterController } from "@airbnb/controller";
 
-export class RegisterConnector extends React.PureComponent {
-  handleSubmit = async (values: any) => {
-    console.log(values);
-    return null;
-  };
-  render() {
-    return <RegisterView submit={this.handleSubmit} />;
-  }
-}
+import { RegisterView } from "./ui/RegisterView";
 
 // container -> view
 // container -> connector -> view
 // controller -> connector -> view
+
+export class RegisterConnector extends React.PureComponent {
+  render() {
+    return (
+      <RegisterController>
+        {({ submit }) => <RegisterView submit={submit} />}
+      </RegisterController>
+    );
+  }
+}
